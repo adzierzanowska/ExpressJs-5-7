b30bfbeeed7456e4487939737d33b6f8980640e5
Amanda Dzierżanowska <dzierzanowska.amanda@gmail.com>
2018-06-15 20:27:43 +0200

    Init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID' : '703320834168-15lregmoj6ofk0sp7f45o09899o63j9o.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET' : 'kP15blp-JGiPdhO7YpgQhFY5',
+    'CALLBACK_URL' : 'http://localhost:3000/auth/google/callback'
+};
